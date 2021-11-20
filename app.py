281875e95466be1e69dbbd27662d566e820b40d9
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-11-20 16:08:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def flask_import():
  <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
   <div id="tradingview_1542b"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="black-text">595 Final Project</span></a> by Group 8</div>
   <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
   <script type="text/javascript">
   new TradingView.widget(
