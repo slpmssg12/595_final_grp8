a3277b8742a532da9e5c8474112b9c45045303ad
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-11-20 15:18:05 -0500

    Create flask.py

---
+++ flask.py
@@ -0,0 +1,66 @@
+
+from flask import Flask,Response , request , flash , url_for,jsonify
+from nltk.tokenize import RegexpTokenizer
+import json
+import re
+import sec_api
+import socketio
+from sec_api import FullTextSearchApi
+from sec_api import XbrlApi
+from sec_api import ExtractorApi
+from sec_api import RenderApi
+
+
+app = Flask(__name__) # "__main__"
+
+
+@app.route('/grp8', methods=['GET', 'POST'])
+def flask_import():
+  return """<html>
+  <head>
+  <title>FA595 Grp 8 Final Project</title>
+</head>
+  <body>
+ <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+  <div id="tradingview_1542b"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script type="text/javascript">
+  new TradingView.widget(
+  {
+  "width": 980,
+  "height": 610,
+  "symbol": "NASDAQ:AAPL",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "light",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": true,
+  "withdateranges": true,
+  "hide_side_toolbar": false,
+  "allow_symbol_change": true,
+  "calendar": true,
+  "studies": [
+    "BB@tv-basicstudies",
+    "Volume@tv-basicstudies"
+  ],
+  "container_id": "tradingview_1542b"
+}
+  );
+  </script>
+</div>
+<!-- TradingView Widget END -->
+</p>
+  </body>
+  </html>
+  """
+
+
+
+  
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080)
